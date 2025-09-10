d4737643fa92cdfdb78ed4156642072393474bd2
Darlington <6.jesus.a@gmail.com>
2025-09-09 22:16:49 -0300

    se hace el navbar con barrita

--- src/Components/Nabvars/Navbar1.tsx
+++ src/Components/Nabvars/Navbar1.tsx
@@ -1,17 +1,19 @@
-import Image from 'next/image'
-import React from 'react'
+'use client'
 
+import { Search } from 'lucide-react';
 const Navbar1 = () => {
+  
+  
   return (
-    <nav className='border-2 border-red-600 h-[80px] flex justify-between items-center pa' >
+    <nav className='border-2 border-red-600 h-[80px] flex justify-between items-center px-4' >
       <div className='text-white'>Logo</div>
       <ul className='text-white flex gap-4'>
-        <li>Element</li>
-        <li>News</li>
-        <li>FAQ</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Lupa icon</li>
+        <li className='nav-link'>Element</li>
+        <li className='nav-link'>News</li>
+        <li className='nav-link'>FAQ</li>
+        <li className='nav-link'>About</li>
+        <li className='nav-link'>Contact</li>
+        <li className='nav-link'><Search className=''/></li>
       </ul>
     </nav>
   )
