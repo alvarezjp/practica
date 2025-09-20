d2e12ecd9c1dc1aac7f6176c5ca4fed294368e66
MjrHp <6.jesus.a@gmail.com>
2025-09-20 19:55:18 -0300

    Agregar el inifiteScroll a la app bookgoogle

--- src/app/Server/serverBookGoogle/ViewServer/ViewBookGoogle.tsx
+++ src/app/Server/serverBookGoogle/ViewServer/ViewBookGoogle.tsx
@@ -1,39 +1,85 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from 'react'
 import { getVolBook } from '../serverBookGoogle'
-import { Item, TypeBook } from '../../Types/TypesBooksGoogle';
+import { Item, TypeBook, VolumeInfo } from '../../Types/TypesBooksGoogle';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const ViewBookGoogle = async () => {
-  const inputData = await getVolBook<TypeBook>('harry potter');
-  const itemsData = inputData.items
-  // console.log("inputData",inputData);
-  // console.log("itemsData",itemsData);
+const ViewBookGoogle =  () => {
 
-  const title = (item:Item) => {
-    return item.volumeInfo.title
+  const [items, setItems] = useState<Item[]>([])
+  const [startIndex, setStartIndex] = useState(0)
+  const [hasMode, setHasMore] = useState(true)
+
+
+  // const inputData = await getVolBook<TypeBook>('harry+potter',startIndex,5);
+  // const itemsData = inputData.items
+
+  const fetchData = async () => {
+    const inputData = await getVolBook<TypeBook>('harry+potter', startIndex, 5);
+    if (!inputData.items || inputData.items.length === 0) {
+      setHasMore(false)
+      return;
+    }
+
+    setItems((prev) => [...prev, ...inputData.items]);
+    setStartIndex((prev) => prev + 5)
   }
-  const pageCount = (item:Item) => {
-    return item.volumeInfo.pageCount
-  }
-  const description = (item:Item) => {
-    return item.volumeInfo.description
-  }
 
- itemsData.map((items) => {
-  console.log(title(items))
- })
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+console.log('esto son los items que tengo',items)
+
+  const typeInfo = <K extends keyof VolumeInfo>(item: Item, key: K): VolumeInfo[K] => {
+    return item.volumeInfo[key]
+  }
   return (
     <div className='text-white'>
       <h1 className='text-white'>Component ViewBookGoogle</h1>
-      {itemsData.map((items) => (
+      <InfiniteScroll
+        dataLength={items.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={hasMode}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        // below props only if you need pull down functionality
+        // refreshFunction={this.refresh}
+        // pullDownToRefresh
+        // pullDownToRefreshThreshold={50}
+        // pullDownToRefreshContent={
+        //   <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+        // }
+        // releaseToRefreshContent={
+        //   <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+        // }
+      >
+        {items.map((items) => (
+          <div key={items.id} className='border border-red-300 mt-8'>
+            <h1>{typeInfo(items, 'title')}</h1>
+            <h2>{typeInfo(items, 'pageCount')}</h2>
+            <h3>{typeInfo(items, 'description')}</h3>
+          </div>
+
+        )
+        )}
+      </InfiniteScroll>
+
+      {/* {itemsData.map((items) => (
         <div key={items.id} className='border border-red-300 mt-8'>
-          <h1>{title(items)}</h1>
-          <h2>{pageCount(items)}</h2>
-          <h3>{description(items)}</h3>
+          <h1>{typeInfo(items, 'title')}</h1>
+          <h2>{typeInfo(items, 'pageCount')}</h2>
+          <h3>{typeInfo(items, 'description')}</h3>
         </div>
-        
+
       )
-      )}
+      )} */}
+
     </div>
   )
 }
--- src/app/Server/serverBookGoogle/serverBookGoogle.ts
+++ src/app/Server/serverBookGoogle/serverBookGoogle.ts
@@ -1,7 +1,7 @@
 const URL_VOLUMEN = "https://www.googleapis.com/books/v1/volumes?q="
 
-export const getVolBook = async <T> (search:string): Promise <T> =>  {
-const res = await fetch (`${URL_VOLUMEN}${search}&maxResults=5`);
+export const getVolBook = async <T> (search:string,index=0,maxResults=5): Promise <T> =>  {
+const res = await fetch (`${URL_VOLUMEN}${search}&startIndex=${index}&maxResults=${maxResults}`);
 if (!res.ok){
     throw new Error('Falla de comunicacion con servidor')
 }
